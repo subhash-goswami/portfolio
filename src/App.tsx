a4f2f417eda2d4035a284f68d5d01da1bcb730fc
Subahsh Goswami <subhashg2310@gmail.com>
2022-12-07 12:13:00 +0530

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
